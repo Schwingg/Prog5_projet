0fc83726a9984bed7cb6f9e6241372f1d67f25c7
thony8 <anthony.chaput@e.ujf-grenoble.fr>
2016-01-05 17:12:54 +0100

    Modification section header

--- sources/section_header.c
+++ sources/section_header.c
@@ -8,7 +8,7 @@
 
 typedef struct
  {
-	int sh_name;
+	char* sh_name;
   int sh_type;
   int sh_flags;
   int sh_addr;
@@ -20,21 +20,69 @@ typedef struct
   int sh_entsize;
 } SEC_HEADER;
 
-SEC_HEADER section_header(FILE* fichier)
+int main()
 {
-	int octet = 0, i = 0;
-	SEC_HEADER *sections;
-  sections = (SEC_HEADER *) malloc(sizeof (SEC_HEADER)*SEC_NBR);
-  if (sections == NULL)
-    return NULL;
+	FILE* fichier;
+	fichier = fopen("example1.o","r");
+
 	
-  fseek(fichier, SEC_HEAD_IDX, SEEK_SET);
+	int pos = 0, i = 0;
+	SEC_HEADER **sections = (SEC_HEADER **) malloc((sizeof (SEC_HEADER))*SEC_NBR);
+  if (sections == NULL)
+    return 1;
+
+  for(i = 0; i < SEC_NBR; i++)
+  {
+		sections[i] = (SEC_HEADER*) malloc(sizeof(SEC_HEADER));
+		if (sections[i] == NULL)
+			return 1;
+	}
+	
+  fseek(fichier, (SEC_NAME_NO*SEC_SIZE)+SEC_HEAD_IDX+17, SEEK_SET); // 5 * 4 - 3 bytes
+  fread(&pos, sizeof(int), 1, fichier);
+
+  fseek(fichier, SEC_HEAD_IDX, SEEK_SET);  
 
 	int idxName = 0;
 	for(i = 0; i < SEC_NBR; i++)
 	{
 		idxName = 0;
+		//sections[i]->sh_name = (char*) malloc
 		fread(&idxName,sizeof(int),1,fichier);
-		if(idxName)
+		if(idxName == 0)
+		{
+			if(i == 0)
+				sections[i]->sh_name = "";
+			//else
+				//return NULL;				
+		}
+		else
+		{
+			fseek(fichier,pos+idxName,SEEK_SET);
+			char c;
+			int j = 0;
+			do
+			{
+				fread(&c,sizeof(char),1,fichier);
+				if(c)
+					sections[i]->sh_name[j] = c;
+				else
+					sections[i]->sh_name[j] = '\0';
+				j++;
+			}while(c != 0);
+			fseek(fichier,SEC_HEAD_IDX+(40*i)+4, SEEK_SET);
+		}
+
+		fread(&sections[i]->sh_type, sizeof(int), 1, fichier);
+		fread(&sections[i]->sh_flags, sizeof(int), 1, fichier);
+		fread(&sections[i]->sh_addr, sizeof(int), 1, fichier);
+		fread(&sections[i]->sh_offset, sizeof(int), 1, fichier);
+		fread(&sections[i]->sh_size, sizeof(int), 1, fichier);
+		fread(&sections[i]->sh_link, sizeof(int), 1, fichier);
+		fread(&sections[i]->sh_info, sizeof(int), 1, fichier);
+		fread(&sections[i]->sh_addralign, sizeof(int), 1, fichier);
+		fread(&sections[i]->sh_entsize, sizeof(int), 1, fichier);
 	}
+
+	return 0;
 }
