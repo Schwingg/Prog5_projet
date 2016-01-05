8a3a27a6154a68793270ba1d70684d60ca65f690
georges <georges@localhost.localdomain>
2016-01-05 11:42:03 +0100

    fermer les fichier

--- sources/read_header.h
+++ sources/read_header.h
@@ -1,7 +1,19 @@
 
 
-
 /*
  contient la fonction permettant de faire la lecture du header
  */
 int read_header(BFILE* bfichier);
+
+/*
+ * permet de sauter un nombre definit d'octet
+ * octet est le noombre d'octet qu'il faut sauter 
+ */
+void jump(int octet);
+
+/*
+ * permet de lire un nombre definit d'octet soit en little soit en big endian
+ * octet indique le nombre d'octet quil faut lire 
+ * endian est la methode dencodage 1 pour little 0 pour big
+ */
+int read(int octet, int endian);
