0c9bcceb1c5fdc0c41a21215f1130b75871d3082
georges <georges@localhost.localdomain>
2016-01-05 08:26:10 +0100

    modification passage parametre fichier

--- sources/projet.c
+++ sources/projet.c
@@ -1,18 +1,16 @@
-
+#include "bfile.h"
+#include "stdio.h"
 
 FILE* fichier;
 BFILE* bfichier;
 
 int main(int argc, char *argv[]) {
-
     FILE *fichier = NULL;
-
-    char caractere;
-
     if (argc > 1) {
-
-        fichier = fopen(argv[1], "r"); /* On ouvre le fichier dont le chemin est accessible via argv[1] */
-
+        fichier = fopen(argv[1], "r");
+        /* On ouvre le fichier dont le chemin est accessible via argv[1] */
+        return;
+    } else {
+        return 1;
     }
-
 }
