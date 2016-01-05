2f6d36b0a823b3da63e0ab90e65bb45323c12a82
georges <georges@localhost.localdomain>
2016-01-05 08:55:45 +0100

    ajout fonction read header

--- sources/projet.h
+++ sources/projet.h
@@ -9,3 +9,9 @@ BFILE* bfichier;
  contient le main du projet avec la recherche du fichier passer en parametre
  */
 int main();
+
+
+/*
+ contient la fonction permettant de faire la lecture du header
+ */
+void read_header();
