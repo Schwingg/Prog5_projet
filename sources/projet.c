3c472105cbdec1efb9f1af35b0491801b5839d29
georges <georges@localhost.localdomain>
2016-01-05 14:34:45 +0100

    pedrte de tems

--- sources/projet.c
+++ sources/projet.c
@@ -1,6 +1,7 @@
 #include "stdio.h"
 #include "read_header.h"
 #include "projet.h"
+#include "stdlib.h"
 
 FILE *fichier = NULL;
 
@@ -9,7 +10,10 @@ void ferme() {
 }
 
 int main(int argc, char *argv[]) {
-
+    HEADER *hed;
+    hed = (HEADER *) malloc(sizeof (HEADER));
+    if (hed == NULL)
+        return 1;
     if (argc == 2) {
         if ((fichier = fopen(argv[1], "r"))) {
             /* Open the file passed in argv[1] */
@@ -17,7 +21,8 @@ int main(int argc, char *argv[]) {
             printf("impossible de lire le fichier\n");
             return 1;
         }
-        if (read_header(fichier)) {// No error during header reading
+        hed = read_header(fichier); // No error during header reading
+        if (hed->ELF == 1) {
             ferme();
             return 0;
         } else {// Header reading returned an error
