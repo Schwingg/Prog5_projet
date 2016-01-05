b4e618973c4fb6590d1bc733f862d2639dc28a1e
georges <georges@localhost.localdomain>
2016-01-05 15:05:23 +0100

    free

--- sources/projet.h
+++ sources/projet.h
@@ -1,14 +1,20 @@
 
 FILE *fichier = NULL;
 
+HEADER *hed;
+
 /*
  Contains the project's main function, with file passed as a parameter
  */
-int main();
+int main(int argc, char *argv[]);
 
 
 /*
  Allows to quit the program, specifying if a problem occurs
  */
-
 void ferme();
+
+/*
+ desalloc the structures used
+ */
+void desalloc();
