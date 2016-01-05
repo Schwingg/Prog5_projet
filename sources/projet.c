b6f5a361ddf2d2d5eba8ef76c6745f965ecfad27
georges <georges@localhost.localdomain>
2016-01-05 08:23:40 +0100

    modification passage parametre fichier

--- sources/projet.c
+++ sources/projet.c
@@ -1,44 +1,18 @@
 
 
-int main (int argc, char *argv[])
+FILE* fichier;
+BFILE* bfichier;
 
-{
+int main(int argc, char *argv[]) {
 
     FILE *fichier = NULL;
 
     char caractere;
 
-    if (argc > 1)
+    if (argc > 1) {
 
-    {
-
-        fichier = fopen (argv[1], "r"); /* On ouvre le fichier dont le chemin est accessible via argv[1] */
+        fichier = fopen(argv[1], "r"); /* On ouvre le fichier dont le chemin est accessible via argv[1] */
 
     }
-
-
-    if (fichier != NULL)
-
-    {
-
-        do
-
-        {
-
-            caractere = fgetc (fichier);
-
-            printf("%c", caractere);
-
-        } while (caractere != EOF);
-
-    }
-
-    else exit(EXIT_FAILURE);
-
-
-    getchar();
-
-
-    return EXIT_SUCCESS;
 
 }
