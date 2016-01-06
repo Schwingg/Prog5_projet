9d2ed5d77e6b36e50fbc0e52a4445fb6acd608f9
georges <georges@localhost.localdomain>
2016-01-06 13:14:40 +0100

    lecture sections

--- sources/read_section.c
+++ sources/read_section.c
@@ -6,28 +6,35 @@
 #include <stdlib.h>
 #include "read_header.h"
 #include "section_header.h"
+#include "read_section.h"
 
 
 // FIXME : Bourrage du dernier octet observé
 
-int read_section() {
-    FILE* fichier = fopen("example1.o", "r");
-    if (fichier == NULL)
-        return 1;
-    int i = 0;
+int read_section(FILE* fichier, SEC_HEADER **sections, HEADER *hed) {
+    int i, j;
     unsigned int hex = 0x0;
-
-    fseek(fichier, SEC_OFF, SEEK_SET);
-
-    for (i = SEC_OFF; i < SEC_OFF + SEC_SIZE; i = i + 4) {
-        fread(&hex, 4, 1, fichier);
-        hex = htobe32(hex);
-        printf("%x ", hex);
-        //printf("%x ", i);
-        hex = 0;
+    for (i = 0; i < hed->e_shnum; i++) {
+        printf("Section %s\n", sections[i]->sh_name);
+        fseek(fichier, htobe32(sections[i]->sh_offset), SEEK_SET);;
+        for (j = htobe32(sections[i]->sh_offset); j < htobe32(sections[i]->sh_offset) + htobe32(sections[i]->sh_size); j = j + 4) {
+            fread(&hex, 4, 1, fichier);
+            hex = htobe32(hex);
+            printf("%08x ", hex);
+            hex = 0;
+        }
+        printf("\n");
     }
-    printf("\n");
-    fclose(fichier);
 
+    //    fseek(fichier, SEC_OFF, SEEK_SET);
+    //
+    //    for (i = SEC_OFF; i < SEC_OFF + SEC_SIZE; i = i + 4) {
+    //        fread(&hex, 4, 1, fichier);
+    //        hex = htobe32(hex);
+    //        printf("%x ", hex);
+    //        //printf("%x ", i);
+    //        hex = 0;
+    //    }
+    //    printf("\n");
     return 0;
 }
