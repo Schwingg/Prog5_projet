dbe7af5045e62bb9a3e2f0703a2a65df95e1c144
Niid <ptitmouls@gmail.com>
2016-01-06 13:56:23 +0100

    Update read_section.c

--- sources/read_section.c
+++ sources/read_section.c
@@ -5,21 +5,21 @@
 #include "read_section.h"
 
 
-// FIXME : Bourrage du dernier octet observé
-
+//Displays each section's content in hex format
 int read_section(FILE* fichier, SEC_HEADER **sections, HEADER *hed) {
     int i, j, k = 0;
     unsigned int hex = 0x0;
-    for (i = 0; i < hed->e_shnum; i++) {
+    for (i = 0; i < hed->e_shnum; i++) {//for each section
         printf("Section %s\n", sections[i]->sh_name);
         fseek(fichier, htobe32(sections[i]->sh_offset), SEEK_SET);
-        ;
         for (j = htobe32(sections[i]->sh_offset); j < htobe32(sections[i]->sh_offset) + htobe32(sections[i]->sh_size); j = j + 4) {
             k++;
             fread(&hex, 4, 1, fichier);
             hex = htobe32(hex);
             printf("%08x ", hex);
             hex = 0;
+            
+            //Return to line
             if (k % 4 == 0) {
                 printf("\n");
             }
