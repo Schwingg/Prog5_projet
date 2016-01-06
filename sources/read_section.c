7cb0a6dec8db4e6905a081df7781e1c35b3b7533
georges <georges@localhost.localdomain>
2016-01-06 13:39:59 +0100

    lecture sections

--- sources/read_section.c
+++ sources/read_section.c
@@ -12,29 +12,24 @@
 // FIXME : Bourrage du dernier octet observé
 
 int read_section(FILE* fichier, SEC_HEADER **sections, HEADER *hed) {
-    int i, j;
+    int i, j, k = 0;
     unsigned int hex = 0x0;
     for (i = 0; i < hed->e_shnum; i++) {
         printf("Section %s\n", sections[i]->sh_name);
-        fseek(fichier, htobe32(sections[i]->sh_offset), SEEK_SET);;
+        fseek(fichier, htobe32(sections[i]->sh_offset), SEEK_SET);
+        ;
         for (j = htobe32(sections[i]->sh_offset); j < htobe32(sections[i]->sh_offset) + htobe32(sections[i]->sh_size); j = j + 4) {
+            k++;
             fread(&hex, 4, 1, fichier);
             hex = htobe32(hex);
             printf("%08x ", hex);
             hex = 0;
+            if (k % 4 == 0) {
+                printf("\n");
+            }
         }
-        printf("\n");
+        k = 0;
+        printf("\n\n");
     }
-
-    //    fseek(fichier, SEC_OFF, SEEK_SET);
-    //
-    //    for (i = SEC_OFF; i < SEC_OFF + SEC_SIZE; i = i + 4) {
-    //        fread(&hex, 4, 1, fichier);
-    //        hex = htobe32(hex);
-    //        printf("%x ", hex);
-    //        //printf("%x ", i);
-    //        hex = 0;
-    //    }
-    //    printf("\n");
     return 0;
 }
