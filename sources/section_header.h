aa807dd729895b172018e8550f7c03af103cf5b0
georges <georges@localhost.localdomain>
2016-01-06 08:17:39 +0100

    ca compile

--- sources/section_header.h
+++ sources/section_header.h
@@ -1,6 +1,6 @@
 
 typedef struct {
-    int sh_name;
+    char * sh_name;
     int sh_type;
     int sh_flags;
     int sh_addr;
@@ -12,13 +12,13 @@ typedef struct {
     int sh_entsize;
 } SEC_HEADER;
 
-FILE* fichier
-        
-HEADER *hed;
-        
+//FILE* fichier;
+
+HEADER* hed;
+
 /*
  * This program permit to get the header of a section.
  * You must provide to the program a HEADER structure that
  * contain all the information of the ELF header.
  */
-SEC_HEADER *section_header(FILE *fichier)
+SEC_HEADER *section_header(FILE *fichier, HEADER *hed);
