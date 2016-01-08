ca231b6ab409361f90682849283ea2ad03b9974c
Alexandre Moulin <alexandre.moulin@e.ujf-grenoble.fr>
2016-01-08 09:57:28 +0100

    -RELA +gestion RELA dans REL

--- sources/rel.h
+++ sources/rel.h
@@ -3,12 +3,8 @@
 typedef struct {
 	unsigned long r_offset ;
 	unsigned long r_info;
+	long r_addend; //To support RELA
+	bool rela;
 } REL;
-
-typedef struct{
-	unsigned long r_offset;
-	unsigned long r_info;
-	long r_addend;
-} RELA;
 
 void display_rel_sections(FILE* fichier, SEC_HEADER** sections, int nbSecs);
